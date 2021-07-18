405b2acec559698f1170b42a660553f0aa6adb00
Jason Scragz <scragz@users.noreply.github.com>
2021-07-17 17:46:11 -0700

    safe

--- bot/cogs/lurkers.py
+++ bot/cogs/lurkers.py
@@ -12,9 +12,12 @@ class LurkersCog(commands.Cog):
     async def purge(self, ctx: commands.Context) -> None:
         if not await self.bot.warn_bot_channel(ctx): return
         print(f"purging lurkers in server {ctx.guild.name}")
-        users = list(filter(lambda member: helper.lookup_role(member.roles, 'approved') is not None, ctx.guild.members))
+        users = list(filter(lambda member: helper.lookup_role(member.roles, 'approved') is None, ctx.guild.members))
         print(f"found {len(users)} lurkers")
         await ctx.send(f"found {len(users)} lurkers")
-        for user in users:
-            ctx.guild.kick(user=user, reason="lurker")
+        await ctx.send(f"{users[0].name}")
+        await ctx.send(f"{users[1].name}")
+        await ctx.send(f"{users[2].name}")
+        #for user in users:
+        #    ctx.guild.kick(user=user, reason="lurker")
 
