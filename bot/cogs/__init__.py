347cc74f64e35bc8a8b184d39b0ec7d2b1f39206
Jason Scragz <scragz@users.noreply.github.com>
2021-08-19 19:00:16 -0700

    add the rest of the cogs

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -7,7 +7,15 @@ from bot.cogs.presence import PresenceCog
 from bot.cogs.realtalk import RealtalkCog
 from bot.cogs.roler import RolerCog
 from bot.cogs.welcome import WelcomeCog
+
 __all__ = [
   'activity',
+  'afterdark',
+  'anime',
+  'lurkers',
+  'novelty',
   'presence',
+  'realtalk',
+  'roler',
+  'welcome',
 ]
